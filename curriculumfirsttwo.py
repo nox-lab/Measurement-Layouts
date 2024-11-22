1aadbb45d68746de14f83758bb4056fb8ff080c7
Talha1337 <taj29@cam.ac.uk>
2024-11-22 10:14:16 +0000

    we made some changes

--- curriculumfirsttwo.py
+++ curriculumfirsttwo.py
@@ -1,5 +1,6 @@
 '''gonna make a curriculum for the 01-xx-xx and 02-xx-xx arenas, using some os path join stuff whatever idk, hopefully will train successfully and changes can be evaluated.
 
-NEED HPC WORKING AS WELL.
+NEED HPC WORKING AS WELL. works !
 
 '''
+
